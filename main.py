a2cef0d882375a44056642a28ca664f360b4a43d
Mike7860 <michal.78.60@gmail.com>
2023-05-13 16:04:13 +0200

    minor changes

--- main.py
+++ main.py
@@ -4,10 +4,6 @@ import requests
 # from instabot import Bot
 # bot = Bot()
 # bot.login(username="username", password="username")
-query = {"client_id": ,
-  "redirect_uri": "https://socialsizzle.herokuapp.com/auth/",
-  "scope": "user_profile",
-  "response_type": "code",
-  "state": 1}
-response = requests.get("https://api.instagram.com/oauth/authorize", params=query)
+access_token = "access_token="
+response = requests.get("https://graph.facebook.com/v9.0/me/accounts?")
 print(response.text)
