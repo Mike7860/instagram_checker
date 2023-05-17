4940d979ba4e82ae995959b2a4dc8d8457f5c35a
Mike7860 <michal.78.60@gmail.com>
2023-05-17 14:01:16 +0200

    skeleton of API request

--- main.py
+++ main.py
@@ -4,6 +4,21 @@ import requests
 # from instabot import Bot
 # bot = Bot()
 # bot.login(username="username", password="username")
-access_token = "access_token="
-response = requests.get("https://graph.facebook.com/v9.0/me/accounts?")
-print(response.text)
+
+data_0 = {"client_id": ,
+          "redirect_uri": "http://",
+          "scope": "user_profile",
+          "response_type": "code"}
+
+data = {"client_id": ,
+        "client_secret": "",
+        #"client_secret": "",
+        "grant_type": "authorization_code",
+        "redirect_uri": "http://",
+        "code": ""}
+#response = requests.post("https://api.instagram.com/oauth/access_token", data=data)
+response = requests.get("")
+print(response.content)
+
+
+#x = requests.post("https://api.instagram.com/oauth/access_token")
