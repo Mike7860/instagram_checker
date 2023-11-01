d785dd1d155e5e47f277b4627e1e2216946769a9
Mike7860 <michal.78.60@gmail.com>
2023-11-01 17:09:53 +0100

    irt

--- main.py
+++ main.py
@@ -71,6 +71,7 @@ def followed_but_not_following():
         soup = BeautifulSoup(f, "html.parser")
         href_tags = soup.find_all(href=True)
 
+
         for href in href_tags:
             current_following_links.append(href.get('href'))
             if href.get('href') not in followers_links:
