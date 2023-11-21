b89d4c9556f8e019c01a47b0ef51142b6013f260
Mike7860 <michal.78.60@gmail.com>
2023-11-21 16:16:55 +0100

    aio

--- main.py
+++ main.py
@@ -73,6 +73,7 @@ def find_bots():
     with open("02_02_2023_followers.txt", "a", encoding='utf-8') as f:
         for new_to_add in new_followers_links_to_add:
             f.write(new_to_add + '\n')
+
     with open("bots.txt", "w", encoding='utf-8') as b:
         for bot in bots:
             b.write(bot + '\n')
